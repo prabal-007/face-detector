3fa53574a53323fabc8f17e3cb8e8f6caa9ee735
Starkk <42775517+prabal-007@users.noreply.github.com>
2020-07-16 22:52:42 +0530

    Add files via upload

---
+++ face-detection.py
@@ -0,0 +1,15 @@
+# chapter - 9 ////  Face detection
+
+import cv2
+
+faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+img = cv2.imread('messi1.jpg')
+grayimg = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+faces = faceCascade.detectMultiScale(grayimg,1.1,4)
+
+for (x,y,w,h) in faces:
+    cv2.rectangle(img, (x,y),(x+w,y+h),(0,144,201),2)
+
+cv2.imshow('detected',img)
+cv2.waitKey(0)
